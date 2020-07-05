396db585830a774c76f5ee33cba8427532217bf6
Tore Aurstad <tore.aurstad@hemit.no>
2020-07-05 22:44:33 +0200

    Videre arbeid med modul 5

--- Globomantics.Core/Models/Person.cs
+++ Globomantics.Core/Models/Person.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Globomantics.Core.Validation;
 
 namespace Globomantics.Models
 {
@@ -10,6 +11,7 @@ namespace Globomantics.Models
         // Person info
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [AgeValidator(Age = 21, ErrorMessage = "You must be 21 or older to apply for a loan.")]
         public string Dob { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
---
+++ Globomantics.Core/Validation/AgeValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Globomantics.Core.Validation
+{
+    public class AgeValidator : ValidationAttribute
+    {
+
+        public int Age { get; set; }
+
+
+        public override bool IsValid(object value)
+        {
+            DateTime dob;
+            if (value != null && DateTime.TryParse(value.ToString(), out dob))
+            {
+                if (dob.AddYears(Age) <= DateTime.Now)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+
+}
