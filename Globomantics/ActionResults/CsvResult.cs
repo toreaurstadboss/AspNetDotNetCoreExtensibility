bf604754846495e8d88fb02b556b8f6b810ab8a2
Tore Aurstad <tore.aurstad@hemit.no>
2020-07-06 22:14:04 +0200

    Modul 6 ferdig

---
+++ Globomantics/ActionResults/CsvResult.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Globomantics.ActionResults
+{
+    public class CsvResult : IActionResult
+    {
+        private IEnumerable sourceData;
+        private string name; 
+
+        public CsvResult(IEnumerable data, string fileName)
+        {
+            this.sourceData = data;
+            this.name = fileName; 
+        }
+
+        public Task ExecuteResultAsync(ActionContext context)
+        {
+            var builder = new StringBuilder();
+            var writer = new StringWriter();
+
+            foreach (var rate in sourceData)
+            {
+                var properties = rate.GetType().GetProperties();
+                foreach (var prop in properties)
+                {
+                    writer.Write(FindPropertyValue(rate, prop.Name));
+                    writer.Write(", ");
+                }
+                writer.WriteLine();
+            }
+
+            var csvBytes = Encoding.ASCII.GetBytes(writer.ToString());
+            context.HttpContext.Response.Headers["content-disposition"]
+                = "attachment; filename=" + name + ".csv";
+            return context.HttpContext.Response.Body.WriteAsync(csvBytes, 0, csvBytes.Length);
+        }
+
+        public static string FindPropertyValue(object item, string propName)
+        {
+            return item.GetType().GetProperty(propName).GetValue(item, null).ToString() ?? "";
+        }
+
+
+    }
+}
---
+++ Globomantics/Theme/ThemeExpander.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Configuration;
+
+namespace Globomantics.Theme
+{
+    public class ThemeExpander : IViewLocationExpander
+    {
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            var activeTheme = context.Values["ACTIVE_THEME"];
+
+            var viewLocationsList = viewLocations.ToList();
+            var expandedLocations = viewLocationsList;
+            int maxViewLocations = viewLocationsList.Count();
+
+            for (int i = 0; i < maxViewLocations; i++)
+            {
+                expandedLocations.Insert(i, viewLocationsList.ElementAt(i).Replace("/Views", string.Format("/Views/Themes/{0}", activeTheme)));
+            }
+
+            return expandedLocations;
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+            var appSettings =
+                (IConfiguration) context.ActionContext.HttpContext.RequestServices.GetService(typeof(IConfiguration));
+            context.Values["ACTIVE_THEME"] = appSettings["AppSettings:ActiveTheme"];
+
+            // throw new NotImplementedException();
+        }
+    }
+}
